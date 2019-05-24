4526e2cdd9ff9182112c60bd9d498db01774df04
Rashmi <venusara@gmail.com>
2019-05-23 20:08:07 -0500

    finished

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,44 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int[] images = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //View overlay = findViewById(R.id.activity_main);
+
+        //overlay.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                //| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                //| View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        Button askButton = findViewById(R.id.ask_button);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rng = new Random();
+                ImageView ball = findViewById(R.id.image);
+
+                ball.setImageResource(images[rng.nextInt(5)]);
+            }
+        });
     }
 }
